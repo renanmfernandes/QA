32206cb1446a13097bb2c72ca5688e4a56d5350c
RenanMFernandes <renan.fernandes@webjump.com.br>
2020-07-13 12:02:33 -0300

    update

--- Trilha_2/features/support/env.rb
+++ Trilha_2/features/support/env.rb
@@ -3,6 +3,7 @@ require "capybara/cucumber"
 require "selenium-webdriver"
 require "faker"
 require "cpf_faker"
+require "report_builder"
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
--- Trilha_2/features/support/hooks.rb
+++ Trilha_2/features/support/hooks.rb
@@ -1,5 +1,32 @@
-After do
-    shot_file = page.save_screenshot("log/screenshot.png")
-    shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
-    embed(shot_b64, "image/png", "Screenshot") # Cucumber anexa o screenshot no report
+# After do
+#     shot_file = page.save_screenshot("log/screenshot.png")
+#     shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+#     embed(shot_b64, "image/png", "Screenshot") # Cucumber anexa o screenshot no report
+#   end
+After do |scenario|
+  Dir.mkdir("data") unless Dir.exist?("data")
+  sufix = ("error" if scenario.failed?) || "success"
+  name = scenario.name.tr(" ", "_").downcase
+  image_name = "data/img/#{sufix}-#{name}.png"
+  temp_shot = page.save_screenshot(image_name)
+  file_shot = File.open(temp_shot, "rb").read
+  final_shot = Base64.encode64(file_shot)
+  embed(temp_shot, "image/png", "Clique aqui para ver a evidência!")
+end
+
+at_exit do
+  time = Time.now
+  data = time.strftime("%d/%m/%Y") #=> "Data 04/09/2011"
+  hora = time.strftime("at %I:%M%p") #=> "08:56AM"
+  ReportBuilder.configure do |config|
+    config.input_path = "data/report.json"
+    config.report_path = "data/cucumber_web_report_e2e"
+    config.report_types = [:html]
+    config.color = "indigo"
+    config.report_tabs = %w[Overview Features Scenarios Errors]
+    config.report_title = "Automação de Testes - Report"
+    config.compress_images = true
+    config.additional_info = { "Projeto" => "Academia - Trilha 2", "Data de execução" => "#{data}" " #{hora}", "QA" => "Renan Fernandes" }
   end
+  ReportBuilder.build_report
+end
