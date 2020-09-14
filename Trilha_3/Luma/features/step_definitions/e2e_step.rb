c552d7f13b695f13445e767ae6a8636b0914b25a
RenanMFernandes <renan.fernandes@webjump.com.br>
2020-09-14 15:35:31 -0300

    0.3.2.3

--- Trilha_3/Luma/features/step_definitions/e2e_step.rb
+++ Trilha_3/Luma/features/step_definitions/e2e_step.rb
@@ -1,49 +1,70 @@
 Dado("que estou com um usuario autenticado no site") do
-    find(".authorization-link", text: "Sign In").click
-    find("#email").set "roni_cost@example.com"
-    find("#pass").set "roni_cost3@example.com"
-    find("#send2").click
-  end
+  @e2e.login_luma
+end
+
+Dado("pesquisar pelo produto SKU {string} e adicionar no carrinho") do |sku|
+  @e2e.busca_produto(sku)
+  @e2e.add_carrinho
+end
+
+Quando("finalizar a compra") do
+  @e2e.finalizar_pedido
+end
+
+Quando("preenchendo com as informaçoes de endereço, metodo de envio, metodo de pagamento") do
+  @e2e.preencher_pedido
+end
+
+Entao("o pedido sera gerado com sucesso") do
+  @e2e.confirmacao_pedido
+end
+
+# Dado("que estou com um usuario autenticado no site") do
+#     # find(".authorization-link", text: "Sign In").click
+#     # find("#email").set "roni_cost@example.com"
+#     # find("#pass").set "roni_cost3@example.com"
+#     # find("#send2").click
+#   end
   
-  Dado("pesquisar pelo produto SKU {string} e adicionar no carrinho") do |sku|
-    find("#search").set sku
-    find("#search").native.send_keys(:enter)
-    find(".product-item-link").click
-    find("#product-addtocart-button").click
-    sleep 2
-  end
+#   Dado("pesquisar pelo produto SKU {string} e adicionar no carrinho") do |sku|
+#     # find("#search").set sku
+#     # find("#search").native.send_keys(:enter)
+#     # find(".product-item-link").click
+#     # find("#product-addtocart-button").click
+#     # sleep 2
+#   end
   
-  Quando("finalizar a compra") do
-    find(".showcart").click
-    assert_selector("#top-cart-btn-checkout")
-    find("#top-cart-btn-checkout").click
-    assert_no_selector(".loader")
-  end
+#   Quando("finalizar a compra") do
+#     # find(".showcart").click
+#     # assert_selector("#top-cart-btn-checkout")
+#     # find("#top-cart-btn-checkout").click
+#     # assert_no_selector(".loader")
+#   end
   
-  Quando("preenchendo com as informaçoes de endereço, metodo de envio, metodo de pagamento") do
-    verificar_endereço = has_css?("input[name='telephone']", wait: 2)
-      if verificar_endereço == true
-      find("input[name='company']").set Faker::Company.name
-      find("input[name='street[0]']").set Faker::Address.street_name
-      find("input[name='street[1]']").set Faker::Address.building_number
-      find("input[name='street[2]']").set Faker::Address.secondary_address
-      find("input[name='city']").set Faker::Address.city
-      find("select[name='country_id'] option[value='BR']").click
-      find("select[name='region_id'] option[value='508']").click
-      find("input[name='postcode']").set Faker::Address.postcode
-      find("input[name='telephone']").set Faker::PhoneNumber.cell_phone_with_country_code
-      assert_no_selector(".loader")
-      end
-      first("input[class='radio']").click
-      find("button[data-role='opc-continue']").click
-      assert_no_selector(".loader")
-      find("button[class*='checkout']").click
-      assert_no_selector(".loader")
+  # Quando("preenchendo com as informaçoes de endereço, metodo de envio, metodo de pagamento") do
+  #   verificar_endereço = has_css?("input[name='telephone']", wait: 2)
+  #     if verificar_endereço == true
+  #     find("input[name='company']").set Faker::Company.name
+  #     find("input[name='street[0]']").set Faker::Address.street_name
+  #     find("input[name='street[1]']").set Faker::Address.building_number
+  #     find("input[name='street[2]']").set Faker::Address.secondary_address
+  #     find("input[name='city']").set Faker::Address.city
+  #     find("select[name='country_id'] option[value='BR']").click
+  #     find("select[name='region_id'] option[value='508']").click
+  #     find("input[name='postcode']").set Faker::Address.postcode
+  #     find("input[name='telephone']").set Faker::PhoneNumber.cell_phone_with_country_code
+  #     assert_no_selector(".loader")
+  #     end
+  #     first("input[class='radio']").click
+  #     find("button[data-role='opc-continue']").click
+  #     assert_no_selector(".loader")
+  #     find("button[class*='checkout']").click
+  #     assert_no_selector(".loader")
       
-  end
+  # end
   
-  Entao("o cadastro sera gerado com sucesso") do
-    order = find(".base").text
-    expect(order).to eql "Thank you for your purchase!"
-    puts find(".order-number").text
-  end
+#   Entao("o cadastro sera gerado com sucesso") do
+#     # order = find(".base").text
+#     # expect(order).to eql "Thank you for your purchase!"
+#     # puts find(".order-number").text
+#   end
--- Trilha_3/Luma/features/support/pages/e2e.rb
+++ Trilha_3/Luma/features/support/pages/e2e.rb
@@ -2,5 +2,71 @@ class E2e
     include Capybara::DSL
     include RSpec::Matchers
 
+    def initialize
+        @nome_companhia=Faker::Company.name
+        @rua=Faker::Address.street_name
+        @numero_rua=Faker::Address.building_number
+        @complemento_rua=Faker::Address.secondary_address
+        @cidade=Faker::Address.city
+        @cep=Faker::Address.postcode
+        @telefone=Faker::PhoneNumber.cell_phone_with_country_code
+        @email="roni_cost@example.com"
+        @senha="roni_cost3@example.com"
+    end
 
+    def acessa_site(site)
+        visit(EL[site])
+    end
+
+    def login_luma
+        find(EL["luma_btn_singin"]).click
+        find(EL["luma_email"]).set @email
+        find(EL["luma_senha"]).set @senha
+        find(EL["luma_btn_login"]).click
+    end
+
+    def busca_produto(sku)
+        find(EL["luma_pesquisa"]).set (sku)
+        find(EL["luma_pesquisa"]).send_keys :enter
+    end
+
+    def add_carrinho
+        find(EL["luma_btn_produto"]).click
+        find(EL["luna_btn_add_carinho"]).click
+        sleep 2
+    end
+
+    def finalizar_pedido
+        find(EL["luma_btn_show_carrinho"]).click
+        find(EL["luma_btn_checkout_1"]).click
+        assert_no_selector(".loader")
+    end
+
+    def preencher_pedido
+        verificar_endereço = has_css?("input[name='telephone']", wait: 2)
+        if verificar_endereço == true
+            find(EL["luma_campo_nome"]).set @nome_companhia
+            find(EL["luma_campo_rua_nome_1"]).set @rua
+            find(EL["luma_campo_rua_numero"]).set @numero_rua
+            find(EL["luma_campo_rua_nome_2"]).set @complemento_rua
+            find(EL["luma_campo_cidade"]).set @cidade
+            find(EL["luma_campo_pais"]).click
+            find(EL["luma_campo_estado"]).click
+            find(EL["luma_campo_cep"]).set @cep
+            find(EL["luma_campo_telefone"]).set @telefone
+            assert_no_selector(".loader")  
+        end
+            find(EL["luma_btn_radio"]).click
+            find(EL["luma_btn_continue"]).click
+            assert_no_selector(".loader")
+            find(EL["luma_btn_checkout_2"]).click
+            assert_no_selector(".loader")
+    end
+
+    def confirmacao_pedido
+        order = find(".base").text
+        expect(order).to eql "Thank you for your purchase!"
+        puts find(".order-number").text
+    end
+    
 end 
