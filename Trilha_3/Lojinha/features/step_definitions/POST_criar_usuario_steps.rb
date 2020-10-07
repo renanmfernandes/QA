293a0d6c9840901f132de04a754eaaef5df2b9e1
RenanMFernandes <renan.fernandes@webjump.com.br>
2020-10-07 18:08:11 -0300

    0.3.3.1

---
+++ Trilha_3/Lojinha/features/step_definitions/POST_criar_usuario_steps.rb
@@ -0,0 +1,21 @@
+Dado("que estou na requisição {string}") do |url|
+    @url = "http://165.227.93.41/lojinha/usuario"
+  end
+  
+  Quando("faco a requisicao com os dados {string}, {string} e {string}") do |nome, login, senha|
+    @body=  {
+            "usuarionome": "Renan Silva",
+            "usuariologin": "renan.silva",
+            "usuariosenha": "123456"
+            }.to_json
+    @response = HTTParty.post @url, :body => @body, :headers => { "Content-Type" => "application/json" }
+  end
+  
+  Entao("e retornado a resposta {string}") do |status_code|
+    puts @response.parsed_response["message"]
+    expect(@response.code).to eql status_code.to_i
+  end
+  
+  Entao("valido a mensagem {string}") do |sucess|
+    expect(@response.parsed_response["message"]).to eql sucess  
+  end
---
+++ Trilha_3/Lojinha/features/support/env.rb
@@ -0,0 +1,35 @@
+require "capybara"
+require "capybara/cucumber"
+require "selenium-webdriver"
+require "faker"
+require "cpf_faker"
+require "report_builder"
+require "rspec"
+require "httparty"
+
+# when "chrome"
+#     @driver = :selenium_chrome
+#   when "headless"
+#     Capybara.register_driver :selenium_chrome_headless do |app|
+#       chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+#         options.add_argument "--headless"
+#         options.add_argument "--disable-gpu"
+#         options.add_argument "--no-sandbox"
+#         options.add_argument "--disable-site-isolation-trials"
+#         options.add_argument "--disable-dev-shm-usage"
+#       end
+#       Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+#     end
+#     @driver = :selenium_chrome_headless
+#   else
+#     puts "Invalid browser"
+#   end
+  
+  Capybara.configure do |config|
+      config.default_driver = :selenium_chrome
+      config.default_max_wait_time = 5
+    end
+  
+  # EL = YAML.load_file("./data/element.yml")
+  
+#   EL = YAML.load_file(File.join(Dir.pwd, "/data/element.yml"))
