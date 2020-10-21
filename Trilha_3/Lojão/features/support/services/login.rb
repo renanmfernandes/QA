de2a7fb6a59f7c19cf9b361016a4d7c9c5a82608
RenanMFernandes <renan.fernandes@webjump.com.br>
2020-10-21 12:20:59 -0300

    3.3.10

--- Trilha_3/Lojão/features/support/services/login.rb
+++ Trilha_3/Lojão/features/support/services/login.rb
@@ -6,15 +6,15 @@ module Rest
         headers "Content-Type" => "application/json"
         base_uri EL["base_uri"]
 
-            def post_fazer_login
-                self.class.post("/login", :body => dados_body_login)
-            end
+        def post_fazer_login
+            self.class.post("/login", :body => dados_body_login)
+        end
 
-            def dados_body_login
-                return body =  {
-                    "usuariologin": "renan.fernandes",
-                    "usuariosenha": "123456"
-                     }.to_json
-            end
+        def dados_body_login
+            return body =  {
+                "usuariologin": "renan.fernandes",
+                "usuariosenha": "123456"
+                }.to_json
+         end
     end
 end
