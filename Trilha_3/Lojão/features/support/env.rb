7b75556442e2b11d3273043a1687249bc4618ba0
Renan Maia Fernandes <62941757+RenanMFernandes@users.noreply.github.com>
2020-10-28 09:42:51 -0300

    3.3.17

--- Trilha_3/Lojão/features/support/env.rb
+++ Trilha_3/Lojão/features/support/env.rb
@@ -11,29 +11,9 @@ require "httparty/response/headers"
 require_relative "spec_helper/spec_rest"
 World(REST)
 
-# when "chrome"
-#     @driver = :selenium_chrome
-#   when "headless"
-#     Capybara.register_driver :selenium_chrome_headless do |app|
-#       chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
-#         options.add_argument "--headless"
-#         options.add_argument "--disable-gpu"
-#         options.add_argument "--no-sandbox"
-#         options.add_argument "--disable-site-isolation-trials"
-#         options.add_argument "--disable-dev-shm-usage"
-#       end
-#       Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
-#     end
-#     @driver = :selenium_chrome_headless
-#   else
-#     puts "Invalid browser"
-#   end
-  
-  Capybara.configure do |config|
-      config.default_driver = :selenium_chrome
-      config.default_max_wait_time = 5
-    end
-  
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.default_max_wait_time = 5
+end
+
 EL = YAML.load_file("./data/element.yml")
-  
-#   EL = YAML.load_file(File.join(Dir.pwd, "/data/element.yml"))
