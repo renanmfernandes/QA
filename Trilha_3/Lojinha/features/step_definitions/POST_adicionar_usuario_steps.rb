001aa79732d02c35799c5858d07b4d1587acf2fe
Renan Maia Fernandes <62941757+RenanMFernandes@users.noreply.github.com>
2020-11-04 15:30:48 -0300

    3.3.0

--- Trilha_3/Lojinha/features/step_definitions/POST_adicionar_usuario_steps.rb
+++ Trilha_3/Lojinha/features/step_definitions/POST_adicionar_usuario_steps.rb
@@ -7,7 +7,7 @@ Dado("que estou na API lojinha") do
   Quando("faco a requisicao com os dados {string}, {string} e {string}") do |nome, login, senha|
     @body=  {
             "usuarionome": "Adriano Oliveira",
-            "usuariologin": "adriana.Oliveira",
+            "usuariologin": "adriano.Oliveira",
             "usuariosenha": "123456"
             }.to_json
     @response = HTTParty.post @adicionar_usuario, :body => @body, :headers => { "Content-Type" => "application/json" }
