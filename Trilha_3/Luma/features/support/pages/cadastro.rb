eecfa4082977ca5c3a5ac1256268a4058e2dfc5a
Renan Maia Fernandes <62941757+RenanMFernandes@users.noreply.github.com>
2021-10-25 21:26:58 -0300

    Formatação

--- Trilha_3/Luma/features/support/pages/cadastro.rb
+++ Trilha_3/Luma/features/support/pages/cadastro.rb
@@ -1,32 +1,30 @@
 class Cadastro
-    include Capybara::DSL
-    include RSpec::Matchers
+  include Capybara::DSL
+  include RSpec::Matchers
 
-    def initialize
-        @primeiro_nome=Faker::Name.first_name
-        @segundo_nome=Faker::Name.last_name
-        @email=Faker::Internet.email
-        @senha="Faker123456"
-    end
+  def initialize
+    @primeiro_nome = Faker::Name.first_name
+    @segundo_nome = Faker::Name.last_name
+    @email = Faker::Internet.email
+    @senha = "Faker123456"
+  end
 
-    def botao_criar_conta
-        click_link_or_button "Create"
-    end
+  def botao_criar_conta
+    click_link_or_button "Create"
+  end
 
-    def acessa_site(site)
-        visit(EL[site])
-    end
+  def acessa_site(site)
+    visit(EL[site])
+  end
 
-    def criar_conta
-        # find(EL["luma_bnt_criar"]).click
-        find(EL["luma_primeiro_nome"]).set @primeiro_nome
-        find(EL["luma_segundo_nome"]).set @segundo_nome
-        find(EL["luma_inscrever"]).check
-        find(EL["luma_email_cadastro"]).set @email
-        find(EL["luma_senha_cadastro"]).set @senha
-        find(EL["luma_senha_confirmacao"]).set @senha
-        find(EL["luma_btn_confirmacao"]).click
-    end
-
+  def criar_conta
+    # find(EL["luma_bnt_criar"]).click
+    find(EL["luma_primeiro_nome"]).set @primeiro_nome
+    find(EL["luma_segundo_nome"]).set @segundo_nome
+    find(EL["luma_inscrever"]).check
+    find(EL["luma_email_cadastro"]).set @email
+    find(EL["luma_senha_cadastro"]).set @senha
+    find(EL["luma_senha_confirmacao"]).set @senha
+    find(EL["luma_btn_confirmacao"]).click
+  end
 end
-
--- Trilha_3/Luma/features/support/pages/view/pedido_gerado.rb
+++ Trilha_3/Luma/features/support/pages/view/pedido_gerado.rb
@@ -1,11 +1,10 @@
 class Pedido_gerado
-    include Capybara::DSL
-    include RSpec::Matchers
+  include Capybara::DSL
+  include RSpec::Matchers
 
-    def confirmacao_pedido
-        order = find(EL["luma_mensagem_base"]).text
-        expect(order).to eql "Thank you for your purchase!"
-        puts find(EL["luma_numero_ordem"]).text
-    end
-
+  def confirmacao_pedido
+    order = find(EL["luma_mensagem_base"]).text
+    expect(order).to eql "Thank you for your purchase!"
+    puts find(EL["luma_numero_ordem"]).text
+  end
 end
